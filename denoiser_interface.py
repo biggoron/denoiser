f0e6020d6b2720d56a7e7b60f8c874c2ab860272
Dan Ringwald <dan.ringwald12@gmail.com>
2020-11-19 16:11:43 +0100

    use gpu if available

--- denoiser_interface.py
+++ denoiser_interface.py
@@ -5,14 +5,15 @@ from denoiser.enhance import get_estimate
 
 class Denoiser:
     def __init__(self, model_path):
-        self.model = torch.load(model_path)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = torch.load(model_path).to(self.device)
         self.model.eval()
         # Replicates the args of the denoiser lib
         self.args = namedtuple('EstimateArgs', ['batch_size', 'streaming', 'dry'])(batch_size=1, streaming=True, dry=0)
         
     def denoise(self, pcm):
-        input_audio = torch.Tensor(pcm_to_numpy(pcm)).unsqueeze(dim=0).unsqueeze(dim=0)
-        output_audio = get_estimate(self.model, input_audio, self.args).numpy()
+        input_audio = torch.Tensor(pcm_to_numpy(pcm)).unsqueeze(dim=0).unsqueeze(dim=0).to(self.device)
+        output_audio = get_estimate(self.model, input_audio, self.args).cpu().numpy()
         return output_audio
         
     
